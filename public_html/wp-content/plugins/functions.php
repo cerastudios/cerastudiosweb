fba21c60af0a78a031ae19d345fd1c5647c450e6
cerastudios <espia423@gmail.com>
2017-10-02 21:44:41 +0200

    primer

---
+++ public_html/wp-content/plugins/functions.php
@@ -0,0 +1,14 @@
+
+<?php
+/*
+Plugin Name: Funciones
+Plugin URI: https://ayudawp.com/
+Description: Plugin para liberar de funciones el fichero <code>functions.php</code> y activarlo a placer (o no) .
+Version: 1.0
+Author: Fernando Tellado
+Author URI: https://tellado.es
+License: GPLv2 o posterior
+*/
+//restablecer url de wordpress
+update_option( 'siteurl', 'http://midominiodeverdad.com' );
+update_option( 'home', 'http://midominiodeverdad.com' );
